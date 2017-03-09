7812e2954de6f875a86541f3ad2df97856757d0c
Tangui Morlier <tangui@tangui.eu.org>
2017-03-09 15:08:36 +0100

    Stats for gits of a github organization

---
+++ github/bin/export_a_git.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+CACHE=$(dirname $0)/../.cache/
+mkdir -p $CACHE
+
+github_url=$1
+
+rm -rf /tmp/exportgit
+mkdir -p /tmp/exportgit
+git clone --bare $github_url /tmp/exportgit
+
+download() {
+	md5=$(echo $1 | md5sum | sed 's/ .*//')
+	if ! test -e $CACHE/$md5 ; then
+		curl -s $1 > $CACHE/$md5.tmp
+		mv $CACHE/$md5.tmp $CACHE/$md5
+	fi
+	cat $CACHE/$md5
+}
+
+function moreauthor {
+	author=$1
+	commit=$2
+	mycache=$CACHE"/author_"$(echo $author | md5sum | sed 's/ .*//')
+	if ! test -e $mycache; then
+		commit_url=$(echo $github_url | sed 's|/github.com/|/api.github.com/repos/|' | sed 's|$|/commits/'$commit'|')
+		author_url=$(download $commit_url | grep -A 10 "author" | grep '"url":' | sed 's/.*"url": "//' | sed 's/".*//' | head -n 1)
+		author_company=$(download $author_url | grep '"company": "' | sed 's/.*"company": "//' | sed 's/".*//')
+	fi
+	cat $mycache
+
+}
+
+git --git-dir=/tmp/exportgit log --date=short | grep -A 1 -B 2 '^Author: ' | tr '\n' ';' | sed 's/;;*/;/g' | sed 's/--;/\n/g' | sed 's/commit /Commit: /' | while read contrib ; do
+	commitid=$(echo $contrib | sed 's/Commit: *//' | sed 's/;.*//')
+	date=$(echo $contrib | sed 's/.*;Date: *//' | sed 's/;.*//')
+	author=$(echo $contrib | sed 's/.*;Author: *//' | sed 's/;.*//')
+	echo -n $github_url";"$commitid";"$date";"$author";"
+	moreauthor "$author" $commitid
+done
---
+++ github/bin/export_organization_gits.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd $(dirname $0)/..
+
+curl -s "https://api.github.com/orgs/"$1"/repos" | grep -A 1 'private": ' | grep '"html_url"' | sed 's/.*": "//' | sed 's/".*//' | while read repo; do 
+	echo $repo
+	bash bin/export_a_git.sh $repo
+done
